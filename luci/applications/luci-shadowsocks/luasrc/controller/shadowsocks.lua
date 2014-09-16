e9a4794147e6599ad2e20422f017a713528c5c96
mtyzaq <mtyzaq@163.com>
2014-09-17 01:53:43 +0800

    Revert "Revert "1""
    This reverts commit 1446999d510125571bf2260dac892963ff77f373.

---
+++ luci/applications/luci-shadowsocks/luasrc/controller/shadowsocks.lua
@@ -0,0 +1,28 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2008 Steven Barth <steven@midlink.org>
+Copyright 2014 HackPascal <hackpascal@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
+module("luci.controller.shadowsocks", package.seeall)
+
+function index()
+	
+	if not nixio.fs.access("/etc/config/shadowsocks") then
+		return
+	end
+
+	local page
+	page = entry({"admin", "proxy", "shadowsocks"}, cbi("shadowsocks"), _("shadowsocks"), 45)
+	page.i18n = "shadowsocks"
+	page.dependent = true
+end
