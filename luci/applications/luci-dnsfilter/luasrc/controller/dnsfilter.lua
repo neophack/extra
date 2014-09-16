1446999d510125571bf2260dac892963ff77f373
mtyzaq <mtyzaq@163.com>
2014-09-17 01:53:27 +0800

    Revert "1"
    This reverts commit a0e9e7a8d0219712106c1da35eca3252572a0676.

--- luci/applications/luci-dnsfilter/luasrc/controller/dnsfilter.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.dnsfilter", package.seeall)
-
-function index()
-	local page
-	page = entry({"admin", "services", "dnsfilter"}, cbi("dnsfilter"), _("DNS-Filter"), 33)
-	page.dependent = true
-end
